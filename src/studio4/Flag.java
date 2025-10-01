d2935ca34d3ed286b13b0ae14b34a30cff85f4f6
Nina <g.iyiyol@wustl.edu>
2025-10-01 14:13:07 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,20 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(0.8,0.8,0.8,0.4);
 		
+		StdDraw.setPenRadius(0.4);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.point(0.5,0.5);
+
+		//StdDraw.setPenColor(Color.BLUE);
+		//StdDraw.filledRectangle(0.1,0.4,0.2,0.8);
+
+		//StdDraw.setPenColor(Color.WHITE);
+		//StdDraw.filledRectangle(0.5, 0.5, 0.2, 0.7);
+
+		//StdDraw.setPenColor(Color.RED);
+		//StdDraw.filledRectangle(0.7, 0.2, 0.4, 0.5);
 	}
 }
